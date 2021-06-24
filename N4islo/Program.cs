0a815abf58388f575a4b96e0db6b5afdc43cd080
xokc4 <42719177+xokc4@users.noreply.github.com>
2021-06-24 17:42:57 +0400

    43

--- N4islo/Program.cs
+++ N4islo/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO.Compression;
+using System.Collections;
 
 namespace N4islo
 {
@@ -10,90 +14,196 @@ namespace N4islo
     {
         static void Main(string[] args)
         {
-            string N = Console.ReadLine();//добавления значения
-            uint n = uint.Parse(N);//конвертирования
-            int[][] muss = GroupsNumbersArr(n);//работа с методом
-            foreach(int[] i in muss)//перебор массива
+            string inputFile = @"inputNumber.txt";
+            IsInputFile(inputFile);
+            Console.WriteLine("Загружаю число из файла...");
+            int number = ReadFile(inputFile);                           //Считываю txt файл
+            int[][] result = new int[ReturnGroupCount(number)][];       //Массив массивов для хранения последовательностей чисел
+            result[0] = new int[] { 1 };                                //Первая последовательность = 1
+            Stopwatch sv = new Stopwatch();                             //Таймер для замера времени выполнения задачи
+
+            Console.WriteLine("Внимание! Число в файле = " + number);
+            Console.WriteLine();
+            Console.WriteLine(
+                "1 - Показать только кол-во групп\n" +
+                "2 - Выполнить алгоритм подсчета групп чисел методом перебора значений\n" +
+                "с последующей архивацией или сохранением в файл\n");
+                
+            switch (Console.ReadLine())
             {
-                Console.WriteLine(i);
-            }
-           
+                #region case "1": //Показать только кол-во групп
+                case "1":
+                    int[] range = new int[number - 2];
+                    sv.Start();
+                    Console.WriteLine("Групп чисел: " + ReturnGroupCount(number));
+                    sv.Stop();
+                    PrintTime(sv);
+                    break;
+                #endregion
 
+                #region case "2": //Выполнить алгоритм подсчета групп чисел методом перебора значений
+                case "2":
+                    sv.Start();
+                    Console.WriteLine("Формирую массив чисел от 1 до вашего числа");
+                    range = new int[number - 2];
+                    range = Enumerable.Range(2, number).ToArray();
 
-            Console.ReadKey();
-
-        }
-        public static int[][] GroupsNumbersArr(uint number)
-        {
-
-            /// Если переданное число ноль, то возвращается пустой список групп
-            if (number == 0)
-            {
-                return Array.Empty<int[]>();
-            }
-            /// Если переданное число единица, то возвращается список групп с одной группой - единицей
-            if (number == 1)
-            {
-                return new int[][] { new int[] { 1 } };
-            }
-            /// Создание массива для групп
-            int[][] groups = new int[(int)Math.Log(number, 2) + 1][];
-            groups[0] = new int[] { 1 };
-            int indexGroup = 1; // Индекс добавляемой группы
-
-            /// Создание массива чисел содержащего все числа от 1 до заданного
-            /// Единица используется как маркер
-            /// Вместо удаления элеменов их значение будет приравниваться нулю
-            /// После сортировки 1 будет разделять удалённые элементы и оставшиеся
-            int[] numbers = new int[number];
-            for (int i = 0; i < number; i++)
-            {
-                numbers[i] = i + 1;
-            }    
-            
-            int[] group = new int[number];//Массив с промежуточными данными
-
-
-            int index1;//Цикл пока в массиве индекс единицы не последений
-            while ((index1 = Array.BinarySearch(numbers, 1)) != number - 1) /// Проверка индекса единицы
-            {
-
-                Array.Copy(numbers, group, number);//Копия элементов в массив группы
-
-                int countGroup = 0;// Количество элементов в группе
-                                    
-                for (int i = index1 + 1; i < number; i++)//Перебор элементов группы. i - индекс проверяемого элемента
-                {
-                    if (group[i] != 0) /// Пропуск удалённых элементов
+                    for (int i = 1; i < result.Length; i++)
                     {
-                        /// Удаление из группы всех элементов кратных проверяемому, кроме его самого
-                        for (int j = i + 1; j < number; j++)
-                            if (group[j] % group[i] == 0)
-                                group[j] = 0;
-
-                        
-                        numbers[i] = 0;//Удаление элемента из массива чисел
-
-                        countGroup++;//Счётчик группы увеличивется
+                        result[i] = ReturnList(range);             //  Находим требуемую последовательность
+                        range = range.Except<int>(result[i]).ToArray();    //  Вычитаем из нашей последовательности полученную последовательность
                     }
+                    sv.Stop();
+                    PrintTime(sv);
 
-                }
-                /// Сортировка массивов после удаления элементов
-                Array.Sort(group);
-                Array.Sort(numbers);
+                    Console.WriteLine("Производим запись результатов в файл output2.txt");
+                    string file = "output2.txt";
+                    SaveToFile(file, result);
+
+                    Console.WriteLine("Сохранение в файл завершено!\n");
+                    Console.WriteLine("Заархивировать полученный файл? \n" +
+                        "1 - Да  " +
+                         "0 - Нет, просто выйти из программы");
+                    switch (Console.ReadLine())
+                    {
+                        case "1":
+                            Compressig(file, "archivedOutput2.zip");
+                            Console.WriteLine("Файл заархивирован. Ищите его в папке с exe файлом");
+                            break;
+                        case "0":
+                            break;
+                    }
+                    break;
+                #endregion
 
                 
-                
-                int[] group2 = new int[countGroup];//Создание массива для добавления в группы
-                Array.Copy(group, Array.BinarySearch(group, 1) + 1, group2, 0, countGroup);//копирование в него значений старше 1
-
-                /// Добавление группы в массив групп
-                groups[indexGroup] = group2;
-                indexGroup++;
-
             }
-            
-            return groups;//Возврат списка групп
+            Console.ReadKey();
         }
+        static int ReadFile(string path)
+        {
+            using (StreamReader sr = new StreamReader(path, Encoding.Default))
+            {
+                return int.Parse(sr.ReadLine());
+            }
+        }//Считывание числа из файла
+        static void IsInputFile(string path)
+        {
+            Random random = new Random();
+            int r = random.Next(50, 1000);
+            if (!File.Exists(path))
+            {
+                Console.WriteLine($"Файл данных inputNumber.txt не найден. Создаю файл по умолчанию");
+                using (StreamWriter sw = new StreamWriter(new FileStream("inputNumber.txt", FileMode.Create, FileAccess.Write)))
+                {
+                    sw.WriteLine(r);
+                }
+            }
+        }//Проверка существования файла ввода
+        static void Compressig(string source, string output)
+        {
+            if (!File.Exists(source)) Console.WriteLine("Файл не существует! Создайте файл для архивации или проверьте правильность пути");
+            else
+            {
+                using (FileStream fs = new FileStream(source, FileMode.Open))
+                {
+                    using (FileStream nf = File.Create(output))
+                    {
+                        using (GZipStream gs = new GZipStream(nf, CompressionMode.Compress))
+                        {
+                            fs.CopyTo(nf);
+                        }
+                    }
+                }
+            }
+        }//Архивация файла стандартными средствами
+        static void SaveToFile(string name, int[][] array)
+        {
+            string fileName = name;
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+            using (StreamWriter sw = new StreamWriter(new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write)))
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    sw.WriteLine(PrintArray(array[i]));
+                    sw.Flush();
+                }
+            }
+        }//Сохранение результата вычисления в файл
+
+        /// <summary>
+        /// Вычисляет последовательность простых чисел методом "Решето Эрастофена"
+        /// </summary>
+        /// <param name="list">Исходный массив битов. ненужные элементы заменябтся на true</param>
+        /// <returns>Возвращает массив, где false = простые числа</returns>
+        static BitArray ErastofenBool(BitArray list)
+        {
+            BitArray arr = new BitArray(list);
+            for (int i = 2; i * 2 < arr.Length; i++)
+            {
+                if (!list[i])
+                {
+                    for (int j = i * 2; j < arr.Length; j += i)
+                    {
+                        arr[j] = true;
+                    }
+                }
+            }
+            return arr;
+        }
+
+        /// <summary>
+        /// Возвращает последовательность чисел не делящихся друг на друга
+        /// </summary>
+        /// <param name="array">Исходный массив для выделения последовательности</param>
+        /// <returns></returns>
+        static int[] ReturnList(int[] array)
+        {
+            int[] buffArray = new int[array.Length];
+            array.CopyTo(buffArray, 0);
+            bool flag = true;
+            for (int i = 0; i * 2 < buffArray.Length; i++)
+            {
+                if (buffArray[i] != 0)
+                {
+                    for (int j = i; j < buffArray.Length; j++)
+                    {
+                        if (buffArray[j] % buffArray[i] == 0 && buffArray[j] / buffArray[i] > 1)
+                        {
+                            buffArray[j] = 0;
+                            flag = false;
+                        }
+                    }
+                }
+            }
+            if (!flag) return ReturnList(buffArray);
+            else return buffArray.Where(x => x != 0).ToArray();        //  Записываем последовательнсть в массив, пропуская нули
+        }
+        static void PrintTime(Stopwatch sv)
+        {
+            Console.WriteLine("Время выполнения алгоритма вычисления: " + sv.Elapsed.TotalSeconds + " сек.");
+        }//Метод выводящий строку с замером времени выполнения алгоритма в секундах.
+        static string PrintArray(int[] array)
+        {
+            string result = null;
+            foreach (int item in array)
+            {
+                result += item + " ";
+            }
+            return result;
+        }//Преобразует одномерный массив в строку
+        static int ReturnGroupCount(int number)
+        {
+            int count = 1;
+            while (number > 1)
+            {
+                number /= 2;
+                count++;
+            }
+            return count;
+        }//Вычисляем количество групп чисел, не делящихся друг на друга
     }
 }
